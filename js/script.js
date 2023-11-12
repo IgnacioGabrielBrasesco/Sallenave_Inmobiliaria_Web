e9ceb6de580f3406e93bf225f23a5f049dab52d6
Ignacio Gabriel Brasesco <brasescoignaciogabriel@gmail.com>
2023-11-12 14:29:15 -0300

    agregue seccion servicios y carpertas de imagenes

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ const menu = document.querySelector("ul");
 
 const boton_hamburguesa = document.querySelector(".hamburguesa");
 
-
-
 boton_hamburguesa.addEventListener("click", ()=> {
     if(!menu.className.includes("active")){
         menu.classList.add("active");
     } else{
         menu.classList.remove("active");
     }
-})
+});
+
+
+
