2709a80cab3e33bf5551520c389b299abeb2628a
Ignacio Gabriel Brasesco <brasescoignaciogabriel@gmail.com>
2023-11-14 16:25:09 -0300

    agregue javaScript en el input  buscador de page venta

--- js/script.js
+++ js/script.js
@@ -11,4 +11,41 @@ boton_hamburguesa.addEventListener("click", ()=> {
 });
 
 
+const input = document.querySelector(".buscador")
+
+input.addEventListener("keyup", (event) => {
+    const value = event.target.value.toLowerCase();
+
+    const cards = document.querySelectorAll(".card");
+    
+    cards.forEach((card) => {
+        if((card.querySelector(".card-title").textContent.toLowerCase().includes(value))){
+            card.style.display = "block";
+            
+            // card.style.order = "0";
+            
+            
+            
+        } else{
+            card.style.display = "none";
+            // card.style.order = "1";
+        };
+    });
+}); 
+
+
+
+// const filtros = document.querySelectorAll(".filtros");
+// const cards = document.querySelectorAll(".card");
+
+// cards.forEach((card) => {
+//     card.addEventListener("click", (e) => {
+//         if(e.target.matches("button")){
+//             const card = e.target.parentElement.parentElement;
+//             console.log(card.querySelector("h5").textContent);
+//         }
+//     })
+// })
+
+
 
